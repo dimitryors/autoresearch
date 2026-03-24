f3f008290de04994d299176641c5e1e3d3d14d5d
Dmitriy <dmitriy@orshnskiy.ru>
2026-03-24 10:00:15 +0300

    experiment

--- train.py
+++ train.py
@@ -333,7 +333,7 @@ class GPT(nn.Module):
             x = block(x, ve, cos_sin, attn_arg)
         x = norm(x)
 
-        softcap = 14
+        softcap = 13
         logits = self.lm_head(x)
         logits = logits.float()
         logits = softcap * torch.tanh(logits / softcap)
